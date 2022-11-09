7352f3bac57800b01e6110190d056537659f6fa9
romrig <romansitorigali@gmail.com>
2022-11-09 15:58:02 -0300

    react-r

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {  } from "./CardWidget";
-import ItemListContainer, {  } from "./ItemListContainer";
-import NavBar, {  } from "./NavBar";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-   <div>
     <NavBar/>
-   </div>
-
-  
+    <ItemListContainer greeting={'saludando'}/>
     </div>
   )
 }
---
+++ src/components/CardWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../components/CardWidget.css'
+
+
+const CardWidget = () => {
+  return (
+   <a href=''><img className='imagedit' src="./imagenes/imagedit.png" alt="" srcset="" /></a>
+  )
+}
+
+export default CardWidget
